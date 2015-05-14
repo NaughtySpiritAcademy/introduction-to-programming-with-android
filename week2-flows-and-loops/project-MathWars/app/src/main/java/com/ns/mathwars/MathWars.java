7f83c40a519ed019b27713288a823ab504705edf
Polina Zhelyazkova <poly_vjk@abv.bg>
2015-05-14 15:30:27 +0300

    Remove methods implementation

--- week2-flows-and-loops/project-MathWars/app/src/main/java/com/ns/mathwars/MathWars.java
+++ week2-flows-and-loops/project-MathWars/app/src/main/java/com/ns/mathwars/MathWars.java
@@ -39,53 +39,10 @@ public class MathWars {
     }
 
     public static int[] calculatePoints(boolean[] player1Answers, boolean[] player2Answers) {
-        int player1Points = 0;
-        for(int i=0; i<player1Answers.length; i++) {
-            boolean answer = player1Answers[i];
-            if(answer) {
-                player1Points = player1Points + 10;
-            } else {
-                player1Points = player1Points - 7;
-            }
-        }
-
-        int player2Points = 0;
-        for(int i=0; i<player2Answers.length; i++) {
-            boolean answer = player2Answers[i];
-            if(answer) {
-                player2Points = player2Points + 10;
-            } else {
-                player2Points = player2Points - 7;
-            }
-        }
-
-//        player1Points = calculatePlayerPoints(player1Answers);
-//        player2Points = calculatePlayerPoints(player2Answers);
-
-        int[] points = new int[]{player1Points, player2Points};
-        return points;
-    }
-
-    private static int calculatePlayerPoints(boolean[] playerAnswers) {
-        int points = 0;
-        for(int i=0; i<playerAnswers.length; i++) {
-            boolean answer = playerAnswers[i];
-            if(answer) {
-                points = points + 10;
-            } else {
-                points = points - 7;
-            }
-        }
-        return points;
+        return new int[]{100, 200};
     }
 
     public static int whoWins(int player1Points, int player2Points) {
-        int winner;
-        if(player1Points > player2Points) {
-            winner = 1;
-        } else {
-            winner = 2;
-        }
-        return winner;
+        return 1;
     }
 }
