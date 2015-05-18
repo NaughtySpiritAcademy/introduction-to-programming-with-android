8977378c9cd711b29147112219e6871f8f807350
Venelin Valkov <venelin@bapplz.com>
2015-05-18 16:02:11 +0300

    Code cleanup

--- week3-collections-and-classes/homework-ToDoList/app/src/main/java/ns/com/todolist/Task.java
+++ week3-collections-and-classes/homework-ToDoList/app/src/main/java/ns/com/todolist/Task.java
@@ -1,13 +1,9 @@
 package ns.com.todolist;
 
-import android.util.Log;
-
-import java.util.Date;
-
 /**
  * Created by Naughty Spirit <hi@naughtyspirit.co>
  * on 5/17/15.
  */
-public class Task{
+public class Task {
 
 }
