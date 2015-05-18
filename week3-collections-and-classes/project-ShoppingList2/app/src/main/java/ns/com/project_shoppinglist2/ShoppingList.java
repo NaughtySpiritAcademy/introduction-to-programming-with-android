9186dc60c10779ac9f11bcde173954887e2c156c
Polina Zhelyazkova <poly_vjk@abv.bg>
2015-05-18 14:09:05 +0300

    Remove Shopping List implementation

--- week3-collections-and-classes/project-ShoppingList2/app/src/main/java/ns/com/project_shoppinglist2/ShoppingList.java
+++ week3-collections-and-classes/project-ShoppingList2/app/src/main/java/ns/com/project_shoppinglist2/ShoppingList.java
@@ -10,30 +10,23 @@ public class ShoppingList {
     HashMap<String, Integer> shoppingList;
 
     public void initialize() {
-        shoppingList = new HashMap<>();
-        shoppingList.put("Krushi", 3);
-        shoppingList.put("Fystyci", 6);
-        shoppingList.put("Semki", 2);
+
     }
 
     public HashMap<String, Integer> getItems() {
-        return shoppingList;
+        return null;
     }
 
     public void addItem(String item, int quantity) {
-        shoppingList.put(item, quantity);
     }
 
     public void removeItem(String item) {
-        shoppingList.remove(item);
     }
 
     public int getQuantity(String item) {
-        return shoppingList.get(item);
+        return 0;
     }
 
     public void updateItem(String oldItem, String newItem, int quantity) {
-        shoppingList.remove(oldItem);
-        shoppingList.put(newItem, quantity);
     }
 }
